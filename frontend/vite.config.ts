e7d3b3c3dd3f56f1686b4611a585a54701fb945e
Danila Danko <br4ch1st0chr0n3@gmail.com>
2026-03-04 16:02:06 +0300

    fix(frontend): remove default vite_api_target value

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  const target = env.VITE_API_TARGET || "http://127.0.0.1:42002";
+  const target = env.VITE_API_TARGET;
 
   return {
     plugins: [react()],
